c5ad978d1713658851a2e6d0f4b6ae02a17d71a0
InnaHTML <mavka0208@gmail.com>
2023-07-01 18:00:53 +0300

    optimized the code with colored words

--- js/ryutaKawashima3.js
+++ js/ryutaKawashima3.js
@@ -3,6 +3,7 @@
 /* ================================================================== */
 
 // ====== слова цветные ========================== //
+
 const quotes = [ 
   'красный', 'оранжевый', 'желтый', 'зеленый', 'голубой','синий',  'фиолетовый',
   'серый', 'черный', 'коричневый', 'розовый', 'белый' ];
@@ -11,54 +12,17 @@ const changeQ = document.getElementById('changeQuotes');
 changeQ.addEventListener('click', mots);
 
 function mots() {
+  const test = document.getElementsByClassName('test');
+  for (let tes of test) {
+    tes.innerText = `${quotes[Math.floor(Math.random() * quotes.length)]}`;
 
-  let a = quotes[Math.floor(Math.random() * quotes.length)];
-  let i = quotes[Math.floor(Math.random() * quotes.length)];
-  let c = quotes[Math.floor(Math.random() * quotes.length)];
-  let d = quotes[Math.floor(Math.random() * quotes.length)];
-  let e = quotes[Math.floor(Math.random() * quotes.length)];
-
-
-var test1 = document.querySelector('.test1');
-var test2 = document.querySelector('.test2');
-var test4 = document.querySelector('.test4');
-var test5 = document.querySelector('.test5');
-var test6 = document.querySelector('.test6');
-
-
-  var r = Math.floor(Math.random()*256)  ;
+    let r = Math.floor(Math.random()*256)  ;
       g = Math.floor(Math.random()*256)  ;
       b = Math.floor(Math.random()*256) ;
-test1.style.color = 'rgb('+ r +','+ g + ',' + b + ')';
-document.querySelector('.test1').innerHTML = a + ' ';
-
-  var r = Math.floor(Math.random()*256)  ;
-      g = Math.floor(Math.random()*256)  ;
-      b = Math.floor(Math.random()*256) ;
-test2.style.color = 'rgb('+ r +','+ g + ',' + b + ')';      
-document.querySelector('.test2').innerHTML = i + ' ';
-
-  var r = Math.floor(Math.random()*256)  ;
-      g = Math.floor(Math.random()*256)  ;
-      b = Math.floor(Math.random()*256) ;
-test4.style.color = 'rgb('+ r +','+ g + ',' + b + ')';
-document.querySelector('.test4').innerHTML = c + ' ';
-
-  var r = Math.floor(Math.random()*256)  ;
-      g = Math.floor(Math.random()*256)  ;
-      b = Math.floor(Math.random()*256) ;
-test5.style.color = 'rgb('+ r +','+ g + ',' + b + ')';
-document.querySelector('.test5').innerHTML = d + ' ';
-
-  var r = Math.floor(Math.random()*256)  ;
-      g = Math.floor(Math.random()*256)  ;
-      b = Math.floor(Math.random()*256) ;
-test6.style.color = 'rgb('+ r +','+ g + ',' + b + ')';
-document.querySelector('.test6').innerHTML = e + ' ';
+      tes.style.color = 'rgb('+ r +','+ g + ',' + b + ')';
+  }
 };
-// ============================================================================ //
 
-   
 // ==============секундомер ============================//
 let timer = document.getElementById('timer');
 let startBtn = document.getElementById('startBtn');
